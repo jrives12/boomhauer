cdb742a92accaf87fb4e71f5d1692ad083dd8398
Josh <joshuarives12@gmail.com>
2025-11-04 18:39:03 -0500

    turns weather.py into a function that can be called

--- weather.py
+++ weather.py
@@ -1,30 +1,33 @@
 import urllib.request
 import json
+import os
+from dotenv import load_dotenv
 
-base_url = "https://api.openweathermap.org/data/3.0/onecall"
-api_key = ""#input("API_KEY: ")
+def get_weather(lat, lon):
+	load_dotenv()
+	OPEN_WEATHER_TOKEN = os.getenv("OPEN_WEATHER_TOKEN")
+	BASE_URL = "https://api.openweathermap.org/data/3.0/onecall"
+	api_key = OPEN_WEATHER_TOKEN
 
-headers = {
-    "Authorization": f"Bearer {api_key}",
-    "Accept": "application/json"
-}
-params = {
-        "lat" : "32.815417",#float(input("Latitude: "))
-        "lon" : "-79.931889",#float(input("Longitude: "))
-        "appid" : api_key
-        }
-        
+	headers = {
+	    "Authorization": f"Bearer {api_key}",
+	    "Accept": "application/json"
+	}
+	params = {
+		"lat" : lat,
+		"lon" : lon,
+		"appid" : api_key
+		}
+		
 
-request_url = f"{base_url}?{urllib.parse.urlencode(params)}" 
+	request_url = f"{BASE_URL}?{urllib.parse.urlencode(params)}" 
 
-req = urllib.request.Request(request_url)
+	req = urllib.request.Request(request_url)
 
-with urllib.request.urlopen(req) as response:
-    data = response.read()
-    encoding = response.info().get_content_charset('utf-8')
-    result = json.loads(data.decode(encoding))
+	with urllib.request.urlopen(req) as response:
+	    data = response.read()
+	    encoding = response.info().get_content_charset('utf-8')
+	    result = json.loads(data.decode(encoding))
+	return result
 
-k = float(result['current']['temp'])
-c = k - 273.15
-f = (9/5) * c + 32
-print("Temp (F): %.2f" % f)
+print(get_weather(32, -72))
