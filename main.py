912e771d298a23d46ca15457df0ddb6afd1eb3b4
Josh <joshuarives12@gmail.com>
2025-11-04 19:05:36 -0500

    added get weather to main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import discord
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from noaa_tides_currents import get_tide
+from weather import get_weather
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
